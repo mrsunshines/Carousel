d71df5ffbf5b5b9986f7ec356e0c4b361f45ca3b
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-22 20:26:03 +0100

    korekty

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 $(function(){
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 10000);
-	function changeSlide() { 
-		var moveFirstSlide;
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});	
-	}
-	var l = document.getElementById('slide_left'),
+	var carouselList = $('#carousel ul'),
+		autoPlay = setInterval(changeSlide, 5000),
+		firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last"),
+		l = document.getElementById('slide_left'),
 		r = document.getElementById('slide_right');
-		
-	$(l).on('click', function moveLeft() {
-		var moveFirstSlide;
+	function changeSlide() {
 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+	}
+	function moveFirstSlide() {
+		firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last"),
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}	
+	$(r).on('click', function changeSlideR(){
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		clearInterval(autoPlay);
+	})	
+	function moveLastSlide() {
+		firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last"),
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft:0});
-	}); 
-	$(r).on('click', function moveRight() {
-		var moveLastSlide;
-		carouselList.animate({'marginLeft':-400}, 500, moveLastSlide);
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
-	});
+	}
+	$(l).on('click', function changeSlideL(){
+		carouselList.animate({'marginLeft': 400}, 500, moveLastSlide);
+		clearInterval(autoPlay);
+	})
 });
